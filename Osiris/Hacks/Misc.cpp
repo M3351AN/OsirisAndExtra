cc6be203b27bd7a612bb1048de687b7f4ef2bc14
渟雲 <QuQ@outlook.it>
2023-08-09 05:34:40 +0800

    BUGFIX

--- Osiris/Hacks/Misc.cpp
+++ Osiris/Hacks/Misc.cpp
@@ -1696,7 +1696,7 @@ void Misc::headshotLine(ImDrawList* drawList) noexcept
     const auto& displaySize = ImGui::GetIO().DisplaySize;
     ImVec2 pos;
     pos.x = displaySize.x / 2.0f;
-    pos.y = displaySize.y / 2.0f - displaySize.y / (2.0f * std::tan(Helpers::deg2rad(localPlayer->isScoped() ? localPlayer->fov() : (config->visuals.fov + 90.0f)) / 2.0f) / 2.0f * acos(-1) / 180.0f)) * std::tan(viewAngles.x * acos(-1) / 180.0f);
+    pos.y = displaySize.y / 2.0f - displaySize.y / (2.0f * std::tan(localPlayer->isScoped() ? localPlayer->fov() : (config->visuals.fov + 90.0f) / 2.0f * M_PI / 180.0f)) * std::tan(viewAngles.x * M_PI / 180.0f);
     const auto color = Helpers::calculateColor(config->misc.recoilCrosshair);
     drawGapLine(drawList, pos, color);
 }
